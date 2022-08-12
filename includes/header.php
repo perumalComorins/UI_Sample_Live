b937bc78755e4eb6fd151d46af19bd375a73bf32
hariajithkumar <ajithkumar@mail.comorins.com>
2022-08-12 19:18:49 +0530

    Header component
    Header component done in this branch(FT-3)

--- includes/header.php
+++ includes/header.php
@@ -1,77 +1,37 @@
-<?php?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200&display=swap" rel="stylesheet">
-    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
-    <link href="http://fonts.cdnfonts.com/css/helvetica-neue-9" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="../css/header.css">
-    <title>Home</title>
-</head>
-<body>
-    <header class='container-fluid'>
-        <div class="container container-1240 rest-padding">
-            <div class='pt-4'>
-                <nav class="navbar navbar-expand-lg navbar-light site-navigation rest-padding">
-                    <a class="navbar-brand site-logo" href="#"><img src="../images/Logo 1 2.png" alt=""></a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse nav-section main-nav" id="navbarNavDropdown">
-                        <ul class="navbar-nav">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">About</a>
-                            </li>
-                            <li class="nav-item menu-item-has-children">
-                                        <a href="javascript:void(0)" class="nav-link">Services</a>
-                                        <ul class="sub-menu">
-                                        <li class="menu-item"><a href="small_business.php" class="nav-link">For small businesses</a></li>
-                                        <li class="menu-item"><a href="home_office.php" class="nav-link">For teleworkers</a></li>
-                                        <li class="menu-item"><a href="services.php" class="nav-link">All the services</a></li>
-                                        </ul>
-                                    </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Sound Engineering</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Career</a>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
-            <div class="content-top">
-                <div class="row main-box rest-margin">
-                    <div class="col-xl-6 col-lg-6 content-text">
-                        <h2>Wishes Come</h2>
-                        <h2>True With Us </h2>
-                        <p>We are a dynamic brand evolving to provide services to make the digital world a better place to encounter with.</p>
-                        <button>Make a wish</button>
-                    </div>
-                    <div class="col-xl-6 col-lg-6 content-img">
-                        <img src="../images/Ellipse 20.png" class="img-1" />
-                        <img src="../images/Rectangle 152.png" class="img-2" />
-                        <img src="../images/Rectangle 151.png" class="img-3"/>
-                    </div>
+<header class="container-fluid reset-padding">
+    <div class="container container-1240 reset-padding">
+        <div class='pt-4'>
+            <nav class="navbar navbar-expand-lg navbar-light site-navigation rest-padding">
+                <a class="navbar-brand site-logo" href="#"><img src="images/Logo 1 2.png" alt=""></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse nav-section main-nav" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About</a>
+                        </li>
+                        <li class="nav-item menu-item-has-children">
+                            <a href="javascript:void(0)" class="nav-link">Services</a>
+                            <ul class="sub-menu">
+                            <li class="menu-item"><a href="small_business.php" class="nav-link">For small businesses</a></li>
+                            <li class="menu-item"><a href="home_office.php" class="nav-link">For teleworkers</a></li>
+                            <li class="menu-item"><a href="services.php" class="nav-link">All the services</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Sound Engineering</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Career</a>
+                        </li>
+                    </ul>
                 </div>
-            </div>
+            </nav>
         </div>
-    </header>
-</body>
-</html>
+    <div>
+</header>
