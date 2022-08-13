e8d2cc9714edd8101d1d06d3465cbe527623756d
hariajithkumar <ajithkumar@mail.comorins.com>
2022-08-13 16:13:51 +0530

    Branch-FT-5
    Footerheighlighted Completed

--- includes/footer-heighlighted.php
+++ includes/footer-heighlighted.php
@@ -1,6 +1,6 @@
 <div class="heighlighted-footer-box">
    <div class="container container-1240px">
-      <div class="row box-content">
+      <div class="row box-content reset-margin">
          <div class="col-8 reset-padding">
             <h4>Wishing to create something for yourself?</h4>
             <p>We help you get a boost towards your vision which you have created for your business. </p>
