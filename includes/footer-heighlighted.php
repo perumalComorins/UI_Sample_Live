36be2cca4f2fdc30d84bcc930f994a02e996afbf
hariajithkumar <ajithkumar@mail.comorins.com>
2022-08-13 16:11:27 +0530

    Branch-FT-5
    Footerheighlighted Completed

--- includes/footer-heighlighted.php
+++ includes/footer-heighlighted.php
@@ -1,11 +1,11 @@
 <div class="heighlighted-footer-box">
    <div class="container container-1240px">
       <div class="row box-content">
-         <div class="col-8">
+         <div class="col-8 reset-padding">
             <h4>Wishing to create something for yourself?</h4>
             <p>We help you get a boost towards your vision which you have created for your business. </p>
          </div>
-         <div class="col-4 align-self-center box-link">
+         <div class="col-4 align-self-center reset-padding">
              <a href="" class="btn-link">Make a wish</a>
          </div>
       </div>
