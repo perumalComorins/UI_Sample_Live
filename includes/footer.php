7f304f3b0ae3dd1250b5302523c20fe64a20f6a5
hariajithkumar <ajithkumar@mail.comorins.com>
2022-08-13 12:08:59 +0530

    Branch FT-4 Completed
    Completed Footer Concept

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,35 @@
-<h1>Web Page footer</h1>
+<footer>
+    <div class="container container-1050">
+        <div class="row main-footer">
+            <div class="col-8 footer-left">
+                <div class="content-footer">
+                    <p>WHO WE ARE</p>
+                    <ul>
+                        <li>Work</li>
+                        <li>Services</li>
+                        <li>Careers</li>
+                        <li>Working Process</li>
+                        <li>Why Choose Us</li>
+                        <li>Legal</li>
+                        <li>Privacy Policy</li>
+                        <li>Cookies Policy</li>
+                        <li>Terms and Conditions</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-4 footer-right">
+                <span class="img-position">
+                    <img src="./Images/Vector (2).png" />
+                    <i class="fa fa-whatsapp" aria-hidden="true"></i>
+                </span>
+                <p>Converge 2 Digital<br/>96, Rajendran Street, Valasarvakkam<br/>Chennai 600087<br/>Tamil Nadu, India</p>
+                <ul>
+                    <li><img src="./Images/Vector (3).png"></li>
+                    <li><img src="./Images/Vector (4).png"></li>
+                    <li><img src="./Images/Vector (5).png"></li>
+                    <li><img src="./Images/Vector (6).png"></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
